99a139f31a3f5d5540f83058a23a1f430ecbd745
Amer Hamdan <amashamdan@gmail.com>
2016-05-27 23:58:29 +0300

    Feat: Added comments to index.html and components.js.
    Comments show code's detials.

--- scripts/components.js
+++ scripts/components.js
@@ -1,10 +1,17 @@
+/* ContainerArea component, the parent component of the text input and output. */
 var ContainerArea = React.createClass ({
+	/* input propoerty added to this.state. It holds the text that will go into the results area. Initially set to an empty string. */
 	getInitialState: function() {
 		return {input: ""};
 	},
+	/* This methods is called from TextArea whenever a change is made in the textarea. */
 	handleChange: function(data) {
+		/* The input property is set to the data read from textarea. */
 		this.setState({input: data});
 	},
+	/* Render function, renders a TextArea and ResultsArea components.
+	handleChange callback is passed via props to TextArea.
+	ResultArea text prop is set to the property input. */
 	render: function() {
 		return (
 			<div>
@@ -15,10 +22,13 @@ var ContainerArea = React.createClass ({
 	}
 });
 
+/* The TextArea component where the user inputs the markdown */
 var TextArea = React.createClass ({
+	/* this method calls handleChange method from ContainerArea. The text in the textarea is passed as parameter. */
 	textChange: function(e) {
 		this.props.onChange(e.target.value);
 	},
+	/* textarea calls textChange method when the text changes. */
 	render: function() {
 		return (
 			<div className="text-area">
@@ -29,11 +39,15 @@ var TextArea = React.createClass ({
 	}
 })
 
+/* ResultArea component. This is where the output is displayed. */
 var ResultArea = React.createClass ({
+	/* This changes the markdown to formatted text using marked library. This function was taken from React's tutorial. */
 	rawMarkup: function() {
 		var rawMarkup = marked(this.props.text.toString(), {sanitize: true});
     	return { __html: rawMarkup };
 	},
+	/* rawMarkup is called from the span element and the returned value is rendered.
+	<span dangerouslySetInnerHTML must be used for the output to be displayed, cannot be named anyhing else. This is in React for security reasons and keep the programmer aware of security concerns. */
 	render: function() {
 		return (
 			<div className="result-area">
@@ -44,4 +58,5 @@ var ResultArea = React.createClass ({
 	}
 })
 
+/* Renders ContainerArea and places it container div. */
 ReactDOM.render(<ContainerArea />, document.getElementById("container"));
